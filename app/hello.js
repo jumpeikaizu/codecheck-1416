c0de53b62974a35c1c8a504d14fc5942342d9d5d
Jumpei Kaizu <jumpei@MacBook-Air.local>
2016-03-10 14:54:32 +0900

    hello

--- app/hello.js
+++ app/hello.js
@@ -4,15 +4,7 @@ function hello(name) {
 * nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
 */
   //ここにHelloworldを実装します。
-  if(name == "World"){
-  	return "Hello World!";
-  }
-  else if(name == "codecheck"){
-	return "Hello codecheck!";
-  }
-  else{
-	return "";
-  }
+  return "Hello" + name + "!";
 }
 
 module.exports = hello;
